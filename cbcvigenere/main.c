e50e34223e7a65b98c81331d149f1a7a80638b16
Subhash Naidu <Subhash@subhashs-imac.local>
2016-09-30 00:15:44 -0400

    Initial

--- cbcvigenere/main.c
+++ cbcvigenere/main.c
@@ -1,15 +1,62 @@
-//
-//  main.c
-//  cbcvigenere
-//
-//  Created by subhash naidu on 9/29/16.
-//  Copyright © 2016 subhash naidu. All rights reserved.
-//
+//------------------------------------------------------------------
+// University of Central Florida
+// CIS3360 - Fall 2016
+// Program Author: Subhash Naidu
+//------------------------------------------------------------------
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char *argv[])
+{
+    char *inputfilename = malloc(sizeof(char));
+    char keyword[11];
+    char vector[11];
+    char cleantxt[5000];
+    char inputchar;
+    FILE *ifp;
+    int i = 0;
+    
+    strcpy(inputfilename, argv[1]);
+    strcpy(keyword, argv[2]);
+    strcpy(vector, argv[3]);
+    
+    ifp = fopen(inputfilename, "r");
+    if (ifp == 0)
+    {
+        puts("Sorry could not open file.");
+        exit(1);
+    }
+    
+    
+    while (inputchar != EOF)
+    {
+        inputchar = getc(ifp);
+        //scanning input from file.
+        if (isupper(inputchar) == 1 && isalpha(inputchar) == 1)
+        {
+            cleantxt[i] = tolower(inputchar);
+            i++;
+        }
+        else if(islower(inputchar) == 1 && isalpha(inputchar) == 1)
+        {
+            cleantxt[i] = inputchar;
+            i++;
+        }
+    }
+    
+    fclose(ifp);
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
