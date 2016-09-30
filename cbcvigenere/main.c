dd0863c393ee1dfa0fa9b5046b2f05e937b14436
Subhash3497 <subhash3497@gmail.com>
2016-09-30 15:58:22 -0400

    Update main.c

--- cbcvigenere/main.c
+++ cbcvigenere/main.c
@@ -7,34 +7,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <ctype.h>
 
+int convert(char c);
 int main(int argc, const char *argv[])
 {
     char *inputfilename = malloc(sizeof(char));
     char keyword[11];
     char vector[11];
     char cleantxt[5000];
-    char inputchar;
-    char workarray;
-    
+    char inputchar = NULL;
+    int workarray[11];
     FILE *ifp;
     int i = 0;
     int j = 0;
-    
+
     strcpy(inputfilename, argv[1]);
     strcpy(keyword, argv[2]);
     strcpy(vector, argv[3]);
-    
+
     ifp = fopen(inputfilename, "r");
     if (ifp == 0)
     {
         puts("Sorry could not open file.");
         exit(1);
     }
-    
-    
+
+
     while (inputchar != EOF)
     {
         inputchar = getc(ifp);
@@ -50,32 +49,33 @@ int main(int argc, const char *argv[])
             i++;
         }
     }
-    
     fclose(ifp);
 
-    
     for (i = 0; i < strlen(cleantxt); i++)
     {
-        if ((i % strlen(vector)) == 0)
+        int k = 0;
+        for (j = (i - strlen(vector)); (i % strlen(vector)) == 0; j++)
         {
-            
-            
-            
+            //workarray[] = convert(cleantxt[i]);
+            printf("%d\n",workarray[k]);
+            k++;
         }
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  
+
+
     return 0;
+}
+
+int convert(char c)
+{
+    int n = -1;
+    char * const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    char *p = strchr(alphabet, c);
+
+    if (p)
+    {
+        n = p - alphabet;
+    }
+    return n;
 }
